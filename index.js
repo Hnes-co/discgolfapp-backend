df6f1c1161b20f7ab394bcde20d401b0ee30b071
Hnesco <hannes.pohjola@gmail.com>
2020-12-03 20:58:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+const { response, request } = require('express')
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Result = require('./models/result')
+
+app.use(express.json()) 
+
+app.use(express.static('build'))
+app.use(cors())
+app.use(morgan('tiny'))
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/results', (req, res) => {
+    Result.find({}).then(results => {
+        res.json(results)
+    })
+})
+
+app.get('/api/results/:id', (req, res, next) => {
+    Result.findById(req.params.id).then(result => {
+        if(result) {
+            res.json(result)
+        }
+        else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+  
+app.post('/api/results', (request, response) => {
+    const body = request.body
+  
+    if (body.name === undefined) {
+        return response.status(400).json({ 
+            error: 'name missing' 
+        })
+    }
+  
+    const result = new Result ({
+        name: body.name,
+        course: body.course,
+        score: body.score
+    })
+  
+    result.save().then(savedResult => {
+        response.json(savedResult)
+    })
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

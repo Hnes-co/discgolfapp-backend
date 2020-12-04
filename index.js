a8062dbf563c1ed7b2c83cf1622e87f76f7670ce
Hnesco <hannes.pohjola@gmail.com>
2020-12-04 03:04:21 +0200

    uibuild

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const Result = require('./models/result')
 
 app.use(express.json()) 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan('tiny'))
 
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/results', (req, res) => {
@@ -35,14 +35,8 @@ app.get('/api/results/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-app.get('/courses', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-app.get('/scores', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-app.get('/links', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.get('/*', (req, res) => {
+    res.sendFile('index.html')
 })
 
   

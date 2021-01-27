181ccdd83028b27b2a9920d1d901294068cc754c
Hnesco <hannes.pohjola@gmail.com>
2021-01-27 18:49:17 +0200

    uibuild

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Course = require('./models/result')
 
 app.use(express.json()) 
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan('tiny'))
 
